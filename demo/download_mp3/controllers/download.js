f4d0f1cc5245cbef1685c7285899bb9c222cc5f8
Felipe Velasquez <felipe.velasquez@apiservice.cl>
2019-09-19 13:41:31 -0300

    Fix intervas

--- demo/download_mp3/controllers/download.js
+++ demo/download_mp3/controllers/download.js
@@ -1,63 +1,81 @@
 const puppeteer = require('puppeteer');
 
 exports.getUrl = getUrlAsync;
+let browser, page;
 
 
 async function getUrlAsync(url) {
   var downloadUrl = "";
-  let browser, page;
-  try {
-    browser = await puppeteer.launch({
-      //headless: false
-      args: ['--no-sandbox', '--disable-setuid-sandbox', '--deterministic-fetch']
-    });
 
+  try {
+    if(!browser){
+      browser = await puppeteer.launch({
+        headless: false
+        //args: ['--no-sandbox', '--disable-setuid-sandbox', '--deterministic-fetch']
+      });
+    }
     page = await browser.newPage();
     page.setCacheEnabled(true);
-    await page.goto('https://ytmp3.cc/');
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
+    await page.goto('https://ytmp3.cc/')
     console.log("Load");
     let input = await page.$("#input");
     await input.click({
       delay: 1
-    });
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
+    })
     console.log("Click input");
     await input.type(url, {
       delay: 1
-    });
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
+    })
     console.log("Url", url);
     await page.click("#submit", {
       delay: 1000
-    });
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
+    })
     console.log("Submit");
     
     
-    await page.waitForSelector("#progress[style='display: none;']");
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
+    await page.waitForSelector("#progress[style='display: none;']")
     console.log("hide Progress");
-    await page.waitForSelector("#title:not(:contains('Please insert a valid video URL'))");
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
-    console.log("Find Download");
-    var downloadUrl = await page.$eval("#buttons > a:nth-child(1)", el => el.href);
-    await page.screenshot({path: 'buddy-screenshot'+ new Date().getTime() +'.png'});
-    console.log(downloadUrl);
-    await page.close();
+
+    return await getUrl(page);
+    
     //await browser.close();
   } catch (ex) {
     console.log(ex);
     downloadUrl = ex;
     await page.close();
     //await browser.close();
+    return downloadUrl;
   }
-  return downloadUrl;
+  
 };
+
+const getUrl = (page) => new Promise((resolve, reject) => {
+  console.log("Entro")
+  const interval = setInterval(async () => {
+    console.log("Interval")
+    name = await page.$eval("#title", el => el.textContent);
+    console.log(name)
+    if(name != "Please insert a valid video URL" ){
+      clearInterval(interval);
+      console.log("Find Download");
+      var downloadUrl = await page.$eval("#buttons > a:nth-child(1)", el => el.href);
+      console.log(downloadUrl);
+      await page.close();
+      resolve(downloadUrl);
+    }
+  },1000);
+})
+
 async function wait(ms) {
   var start = new Date().getTime();
   var end = start;
   while (end < start + ms) {
     end = new Date().getTime();
+  }
+}
+
+async function asyncFor(total, callback) {
+  for (let index = 0; index < total; index++) {
+    await callback(index)
   }
 }
